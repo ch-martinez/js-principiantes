ddf7ce7f4eccf0b1a821f0bac679388bfa0ef14b
Christian Martinez <chmartinez.cba@gmail.com>
2023-03-06 19:44:10 -0300

    Promesas

---
+++ Clase 36 - Promesas/scripts.js
@@ -0,0 +1,38 @@
+//MAS INFO SOBRE PROMESAS: https://es.javascript.info/promise-basics
+
+//Una promesa es un objeto con 2 callbacks internos
+const users = [{ id: 1, name: 'Dorian' }, { id: 2, name: 'Laura' }, { id: 3, name: 'Carlos' }]
+const emails = [{ id: 1, email: 'dorian@gmail.com' }, { id: 2, email: 'laura@gmail.com' }]
+
+const getUser = (id) => {
+    const user = users.find(user => user.id == id)
+    const promise = new Promise((resolve, reject) => {
+        if (!user) reject(`Doesn't exist an user with id ${id}`)
+        else resolve(user)
+    })
+    return promise // El return se puede colocar en lugar del "const", se ve en la proxima function
+}
+
+const getEmail = (user) => {
+    const email = emails.find(email => email.id == user.id)
+    return promise = new Promise((resolve, reject) => {
+        if (!email) reject(`${user.name} hasn't email`)
+        else resolve({
+            id: user.id,
+            name: user.name,
+            email: email.email
+        })
+    })
+}
+
+/* getUser(2)
+    .then(user => getEmail(user))
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+*/
+
+//Forma simplificada
+getUser(2)
+    .then(getEmail)
+    .then(console.log)
+    .catch(console.log)
